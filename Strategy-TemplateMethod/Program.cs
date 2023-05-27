5d07e64f243c5df487894cf6f767b3df4a906d71
Анастасия <onastja02@gmail.com>
2023-05-27 11:51:40 +0300

    Добавьте файлы проекта.

---
+++ Strategy-TemplateMethod/Program.cs
@@ -0,0 +1,125 @@
+﻿// Клиентский код выбирает конкретную стратегию
+int[] array = { 20, 5, 1, -78, 12, 2 };
+Context context = new(array);
+
+QuickSortStrategy sort1 = new();
+Client.ClientCode(context, sort1);
+
+MergeSortStrategy sort2 = new();
+Client.ClientCode(context, sort2);
+
+
+class Client
+{
+    public static void ClientCode(Context context, IStrategy strategy)
+    {
+        context.SetStrategy(strategy);
+        context.Sort();
+    }
+}
+
+// Контекст определяет интерфейс, представляющий интерес для клиентов
+class Context
+{
+    // Контекст хранит ссылку на один из объектов Стратегии
+    private IStrategy _strategy;
+    private int[] array;
+
+    public Context(int[] array)
+    {
+        this.array = array;
+    }
+
+    public void SetStrategy(IStrategy strategy)
+    {
+        this._strategy = strategy;
+    }
+
+    // Контекст делегирует некоторую работу объекту Стратегии.
+    public void Sort()
+    {
+        int[] result = this._strategy.DoSortAlgorithm(this.array);
+        for (int i = 0; i < result.Length; i++)
+            Console.Write($"{result[i]} ");
+        Console.WriteLine("\n");
+    }
+}
+
+// Интерфейс Стратегии
+public interface IStrategy
+{
+    int[] DoSortAlgorithm(int[] data);
+}
+
+// Конкретные Стратегии
+class QuickSortStrategy : IStrategy
+{
+    int[] quickAlgorithm(int[] array, int first, int last)
+    {
+        if (array.Length < 2) return array;
+        int pivot = array[last];
+        int pIndex = first;
+        for (int i = first; i < last; i++)
+            if (array[i] <= pivot)
+            {
+                (array[i], array[pIndex]) = (array[pIndex], array[i]);
+                pIndex++;
+            }
+        (array[last], array[pIndex]) = (array[pIndex], array[last]);
+        if (pIndex > 0) quickAlgorithm(array, 0, pIndex - 1);
+        if (last > pIndex) quickAlgorithm(array, pIndex + 1, last);
+        return array;
+    }
+    public int[] DoSortAlgorithm(int[] array)
+    {
+        Console.WriteLine("Производится быстрая сортировка..");
+        quickAlgorithm(array, 0, array.Length - 1);
+        return array;
+    }
+}
+
+class MergeSortStrategy : IStrategy
+{
+    int[] mergeAlgorithm(int[] array, int first, int last)
+    {
+        if (last - first < 1) return array;
+
+        if (last - first == 1)
+            if (array[first] > array[last])
+                (array[first], array[last]) = (array[last], array[first]);
+
+        mergeAlgorithm(array, first, first + (last - first) / 2);
+        mergeAlgorithm(array, first + (last - first) / 2 + 1, last);
+
+        int[] result = new int[last - first + 1];
+        int x = 0;
+        int i = first;
+        int j = first + (last - first) / 2 + 1;
+        while (i <= first + (last - first) / 2 && j <= last)
+        {
+            if (array[j] < array[i])
+                result[x++] = array[j++];
+            else
+                result[x++] = array[i++];
+        }
+        while (i <= first + (last - first) / 2)
+            result[x++] = array[i++];
+        while (j <= last)
+            result[x++] = array[j++];
+        for (int k = first; k <= last; k++)
+            array[k] = result[k - first];
+        return array;
+    }
+    public int[] DoSortAlgorithm(int[] array)
+    {
+        Console.WriteLine("Производится сортировка слиянием..");
+        mergeAlgorithm(array, 0, array.Length - 1);
+        return array;
+    }
+}
+
+//Производится быстрая сортировка..
+//-78 1 2 5 12 20
+
+//Производится сортировка слиянием..
+//-78 1 2 5 12 20
---
+++ TemplateMethod/Program.cs
@@ -0,0 +1,128 @@
+﻿SpaghettiBolognese spaghetti = new();
+Client.ClientCode(spaghetti);
+
+CaesarSalad salad = new();
+Client.ClientCode(salad);
+
+
+class Client
+{
+    public static void ClientCode(Recipe recipe)
+    {
+        recipe.Cook();
+        Console.WriteLine();
+    }
+}
+
+abstract class Recipe
+{
+    public void Cook()
+    {
+        Prepare();
+        CookIngredient();
+        AddSauce();
+        Garnish();
+        Serve();
+    }
+
+    protected abstract void Prepare();
+
+    protected abstract void CookIngredient();
+
+    protected abstract void AddSauce();
+
+    protected virtual void Garnish()
+    {
+        Console.WriteLine("No garnish added");
+    }
+
+    protected abstract void Serve();
+}
+
+class SpaghettiBolognese : Recipe
+{
+    protected override void Prepare()
+    {
+        Console.WriteLine("Preparing spaghetti bolognese...");
+        Console.WriteLine("Boil spaghetti and set aside");
+        Console.WriteLine("Chop onion, garlic, and carrots");
+    }
+
+    protected override void CookIngredient()
+    {
+        Console.WriteLine("Heat oil in a pan and fry onions and garlic");
+        Console.WriteLine("Add minced meat and cook until browned");
+        Console.WriteLine("Add chopped carrots and cook for 5 minutes");
+        Console.WriteLine("Add canned tomatoes, tomato paste, and water");
+        Console.WriteLine("Add Italian herbs and black pepper to taste");
+        Console.WriteLine("Cook for 20-30 minutes until sauce thickens");
+    }
+
+    protected override void AddSauce()
+    {
+        Console.WriteLine("Pour the bolognese sauce over the spaghetti");
+    }
+
+    protected override void Garnish()
+    {
+        Console.WriteLine("Sprinkle grated Parmesan cheese on top");
+    }
+
+    protected override void Serve()
+    {
+        Console.WriteLine("Serve hot");
+    }
+}
+
+class CaesarSalad : Recipe
+{
+    protected override void Prepare()
+    {
+        Console.WriteLine("Preparing Caesar salad...");
+        Console.WriteLine("Wash and tear the lettuce into bite-size pieces");
+        Console.WriteLine("Prepare the croutons");
+    }
+
+    protected override void CookIngredient()
+    {
+        Console.WriteLine("Fry bacon until crisp and chop into small pieces");
+        Console.WriteLine("Boil an egg for 1 minute, cool and chop it");
+    }
+
+    protected override void AddSauce()
+    {
+        Console.WriteLine("Add Caesar dressing and toss well");
+    }
+
+    protected override void Garnish()
+    {
+        Console.WriteLine("Add the bacon, egg, and croutons on top");
+    }
+
+    protected override void Serve()
+    {
+        Console.WriteLine("Serve chilled");
+    }
+}
+
+//Preparing spaghetti bolognese...
+//Boil spaghetti and set aside
+//Chop onion, garlic, and carrots
+//Heat oil in a pan and fry onions and garlic
+//Add minced meat and cook until browned
+//Add chopped carrots and cook for 5 minutes
+//Add canned tomatoes, tomato paste, and water
+//Add Italian herbs and black pepper to taste
+//Cook for 20-30 minutes until sauce thickens
+//Pour the bolognese sauce over the spaghetti
+//Sprinkle grated Parmesan cheese on top
+//Serve hot
+
+//Preparing Caesar salad...
+//Wash and tear the lettuce into bite-size pieces
+//Prepare the croutons
+//Fry bacon until crisp and chop into small pieces
+//Boil an egg for 1 minute, cool and chop it
+//Add Caesar dressing and toss well
+//Add the bacon, egg, and croutons on top
+//Serve chilled
